4462fbbcf8dc3b4e7380d5c45026b1772313e461
ArinKumarJoshi <arinkumarjoshi2004@gmail.com>
2025-07-15 19:39:44 +0530

    added back the /api in axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development"
-        ? "http://localhost:5001/api"
-        : "https://fullstack-chat-app-e6ut.onrender.com", // Update this line
+    baseURL: import.meta.env.MODE === "development" 
+        ? "http://localhost:5001/api" 
+        : "https://fullstack-chat-app-e6ut.onrender.com/api", // Update this line
     withCredentials: true,
 });
