b258e82ee5af14f0b45690ad3be56ebd0184e151
ArinKumarJoshi <arinkumarjoshi2004@gmail.com>
2025-07-15 19:30:05 +0530

    updated the url in axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.MODE === "development"
         ? "http://localhost:5001/api"
-        : "https://fullstack-chat-app-e6ut.onrender.com/api", // Update this line
+        : "https://fullstack-chat-app-e6ut.onrender.com", // Update this line
     withCredentials: true,
 });
